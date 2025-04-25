ea71ee1aa311e39047f92f63a22b5f08830c17a3
Shree Nandan Das <nayan@Shrees-MacBook-Air.local>
2025-04-25 18:30:46 +0530

    Changes done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginStatus, setLoginStatus] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+   if(username === 'user' && password === 'password'){
+      setLoginStatus(true);
+     
+    }
+    else{
+      setLoginStatus(false);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Login Page</h1>
+        {loginStatus? 
+        <h2 >Welcome {username}</h2> :
+
+        loginStatus == false?
+        <>
+         <h2 style={{color: 'red'}}>Invalid uername or password</h2>
+        <form>
+          <label>
+            Username:
+          
+          </label>
+          <input type="text" name="username" required={true} value={username} onChange={(e)=> setUsername(e.target.value)}/>
+          <br />
+          <label>
+            Password:
+        
+          </label>
+          <input type="password" name="password" required={true} value={password} onChange={(e)=> setPassword(e.target.value)}/>
+          <br />
+          <button type="submit" onClick={handleSubmit}> Submit </button>
+        </form>
+        </>
+       : <form>
+       <label>
+         Username:
+       
+       </label>
+       <input type="text" name="username" required={true} value={username} onChange={(e)=> setUsername(e.target.value)}/>
+       <br />
+       <label>
+         Password:
+     
+       </label>
+       <input type="password" name="password" required={true} value={password} onChange={(e)=> setPassword(e.target.value)}/>
+       <br />
+       <button type="submit" onClick={handleSubmit}> Submit </button>
+     </form>
+
+      }
+        
       </header>
     </div>
   );
